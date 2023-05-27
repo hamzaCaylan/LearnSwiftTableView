a43783ef5333851b968f95b3df537271b01fee12
hamzaçaylan <hamzacaylan021@gmail.come>
2023-05-27 11:08:18 +0300

    Initial Commit

---
+++ LearTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearTableView
+//
+//  Created by Hamza Caylan on 27.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
